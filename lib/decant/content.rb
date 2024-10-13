8574cd2cfe5ea67609f70f226de586db063b7335
Ben Pickles <spideryoung@gmail.com>
2024-10-13 17:13:07 +0100

    API docs (#6)

--- lib/decant/content.rb
+++ lib/decant/content.rb
@@ -5,18 +5,41 @@ require_relative 'file'
 module Decant
   class Content < File
     class << self
+      # When using {Decant.define} the returned {Content} subclass comes with
+      # its own {Collection} instance.
+      #
+      # @return [Collection]
       attr_reader :collection
 
+      # Return all matching files within {.collection}.
+      #
+      # @return [Array<Content>]
       def all
         collection.entries.map { |path| new(path) }
       end
 
+      # Find a file within the {.collection} by passing its relative path.
+      #
+      # @example Find a specific nested file within a content directory
+      #   Page = Decant.define(dir: 'content', ext: '.md')
+      #
+      #   # Return an instance for the file `content/features/nesting.md`.
+      #   Page.find('features/nesting')
+      #
+      # @param pattern [String]
+      #
+      # @return [Content]
+      #
+      # @raise [FileNotFound] if a matching file cannot be found
       def find(pattern)
         path = collection.find(pattern)
         raise FileNotFound, %(Couldn't find "#{pattern}" in "#{collection.dir}") unless path
         new(path)
       end
 
+      # Define convenience frontmatter readers - see {Decant.define}.
+      #
+      # @param attrs [Array<Symbol>] a list of convenience frontmatter readers.
       def frontmatter(*attrs)
         attrs.each do |name|
           define_method name do
@@ -26,6 +49,16 @@ module Decant
       end
     end
 
+    # The extension-less relative path of the file within its collection.
+    #
+    # @example
+    #   Page = Decant.define(dir: 'content', ext: 'md')
+    #
+    #   page = Page.find('features/slugs')
+    #   page.path.expand_path # => "/Users/dave/my-website/content/features/slugs.md"
+    #   page.slug             # => "features/slugs"
+    #
+    # @return [String]
     def slug
       self.class.collection.slug_for(path)
     end
